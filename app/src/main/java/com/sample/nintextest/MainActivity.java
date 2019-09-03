8f0fe32a2e78a09e4be32299efa900be8ef2f152
Mridul Gupta <mridul.gupta@gmail.com>
2019-09-04 09:05:51 +1000

    First commit

---
+++ app/src/main/java/com/sample/nintextest/MainActivity.java
@@ -0,0 +1,25 @@
+package com.sample.nintextest;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        replaceFragment(new FragmentSearch(), R.id.fragment_container);
+    }
+
+    private void replaceFragment(Fragment fragment, int frameId) {
+        final FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(frameId, fragment);
+        transaction.commitAllowingStateLoss();
+    }
+}
